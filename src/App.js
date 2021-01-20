912bb44e8c45a6bf1f28ffbaebb3575c02ca4b22
david-christian <a2266565693@gmail.com>
2021-01-21 01:55:24 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoItem from './TodoItem'
+import React, { useState, useRef } from 'react'
 
-function App() {
+const filterStandard = {
+  all: () => true, 
+  done: (todo) => todo.isDone, 
+  undone: (todo) => !todo.isDone, 
+}
+
+export default function App() {
+  const [todos, setTodos] = useState([])
+  const [value, setValue] = useState("")
+  const [filter, setFilter] = useState("all")
+  const id = useRef(1)
+
+  // 新增 todo
+  const handleButtonClick = () => {
+    setTodos([{
+      id:id.current, 
+      content: value, 
+      isDone: false
+    }, ...todos])
+    setValue('')
+    id.current++
+  }
+
+  // 清空 todo
+  const handleClearTodo = () => {
+    setTodos([])
+  }
+
+  // 綁定輸入框
+  const handleInputChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  // 已完成與未完成
+  const handleToggleIsDone = id => {
+    setTodos(todos.map(todo => {
+      if(todo.id !== id) return todo
+      return {
+        ...todo, 
+        isDone: !todo.isDone
+      }
+    }))
+  }
+
+  // 刪除 todo
+  const handleDeleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  const handleFilterTodo = status => {
+    setFilter(status)
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="todo" value={value} onChange={handleInputChange} />
+      <button onClick={handleButtonClick}>Add todo</button>
+      <button onClick={handleClearTodo}>清空</button>
+      <button onClick={() => handleFilterTodo("all")}>ALL</button>
+      <button onClick={() => handleFilterTodo("done")}>已完成</button>
+      <button onClick={() => handleFilterTodo("undone")}>未完成</button>
+      {
+        todos.filter(filterStandard[filter]).map(todo => <TodoItem key={todo.id} todo={todo} handleDeleteTodo={handleDeleteTodo} handleToggleIsDone={handleToggleIsDone} />)
+      } 
     </div>
   );
 }
-
-export default App;
